1335326eaf0bf7a6ed2050292fbc53eae674555a
AHMilano <aaron.milano@outlook.com>
2020-07-06 10:04:55 -0500

    prueba primer commit aaron

--- SolutionBackEnd/Project.DataAccess/Encriptar.cs
+++ SolutionBackEnd/Project.DataAccess/Encriptar.cs
@@ -12,7 +12,7 @@ namespace Milano.BackEnd.DataAccess
     /// </summary>
     public static class Encriptar
     {
-
+        // aaron
         private static byte[] key = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
         private static byte[] iv = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
